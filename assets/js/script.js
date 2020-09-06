1f98066ab9c6cf20cb0b8bd62caf87d1917cf224
ASC8 <asamcent@gmail.com>
2020-09-06 01:41:37 -0700

    hour checker function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,30 @@ $(document).ready( function () {
         console.log(time);
         console.log(value);
     });
+
+    function hourChecker () {
+        var currentHour = moment().hours();
+
+        // go through the time blocks
+        $(".time-block").each(function () {
+            var hourBlock = parseInt($(this).attr("id").split("-")[1]);
+
+            if (hourBlock < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (hourBlock === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+    });
+}
+
+hourChecker();
+
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
