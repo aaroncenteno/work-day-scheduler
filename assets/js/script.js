528f6a1c3ff8b8cbc195fd6015f6cc2f072e768a
ASC8 <asamcent@gmail.com>
2020-09-06 02:15:46 -0700

    time changed to 24 hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,5 +35,19 @@ $(document).ready( function () {
 
 hourChecker();
 
+// interval to check if the hour needs to move
+var interval = setInterval(hourChecker, 15000);
+
+// load saved date from local storage
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
