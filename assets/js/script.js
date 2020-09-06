b86116a00ee831e70c6c7ea6ab810a62d41cdbf7
ASC8 <asamcent@gmail.com>
2020-09-06 01:36:02 -0700

    save button functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready( function () {
 
+    // Save Button Event Listener
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save Items to local storage
+        localStorage.setItem(time, value);
+        console.log(time);
+        console.log(value);
+    });
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
